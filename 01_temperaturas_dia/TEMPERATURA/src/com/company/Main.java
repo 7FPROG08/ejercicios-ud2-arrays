28e37059e6075e4f31a94448c8d7ea2846fe1bd0
Unknown <susalvat@hotmail.com>
2016-11-14 20:40:49 +0100

    empezando ejercicio 1

---
+++ 01_temperaturas_dia/TEMPERATURA/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+
+Pide al usuario las temperaturas de un día (un valor cada hora durante 24 horas) y almacénalas en un array.
+
+Usando los datos contenidos en el array, calcula la temperatura media, la máxima y la mínima.
+
+Visualiza la información en forma de gráfico de barras. Por ejemplo:
+         */
+
+
+        double[] temperaturas = new double[24];
+
+        System.out.println("Temperaturas de un día");
+
+        for (int i = 0; i < 24; i++) {
+            
+
+        }
+    }
+}
