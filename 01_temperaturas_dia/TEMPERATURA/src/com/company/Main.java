76b071065e875b8f1d129efe48a488e7da793062
Unknown <susalvat@hotmail.com>
2016-11-14 21:50:17 +0100

    Primera aproximación

--- 01_temperaturas_dia/TEMPERATURA/src/com/company/Main.java
+++ 01_temperaturas_dia/TEMPERATURA/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
 
         /*
 
@@ -13,14 +21,78 @@ Usando los datos contenidos en el array, calcula la temperatura media, la máxim
 Visualiza la información en forma de gráfico de barras. Por ejemplo:
          */
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
         double[] temperaturas = new double[24];
 
-        System.out.println("Temperaturas de un día");
+        for (int i = 0; i < 24; i++) {
+            System.out.println("Escribe la temperatura: ");
+            temperaturas[i] = Double.parseDouble(br.readLine());
+        }
+
+        double total = 0;
+        double max = Double.MIN_VALUE;
+        double min = Double.MAX_VALUE;
 
         for (int i = 0; i < 24; i++) {
-            
+            total += temperaturas[i];
+
+            if(temperaturas[i] > max){
+                max = temperaturas[i];
+            }
+
+            if (temperaturas[i] < min){
+                min = temperaturas[i];
+            }
+
 
         }
-    }
+
+
+        double media = total/24;
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i);
+            System.out.print(" ");
+
+            for (int j = 0; j < temperaturas[i]; j++) {
+                System.out.print("*");
+
+            }
+            System.out.print(" ");
+
+            System.out.print(temperaturas[i]);
+
+        if (temperaturas[i] == max){
+            System.out.print(" MAX");
+        }
+
+        if (temperaturas[i] == min){
+            System.out.print(" MIN");
+        }
+
+            System.out.println();
+
+
+        }
+
+
+        System.out.println("la media: "+media);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 }
